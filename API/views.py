f38de64a7ca61faad04e8f0046106953ecc1ee98
Raúl López Llana <raul.lopez@edu.uah.es>
2023-11-21 14:04:01 +0100

    separate functions

--- API/views.py
+++ API/views.py
@@ -6,7 +6,55 @@ import os
 from API.models import Users
 import hashlib
 import requests
-from twofa.views import twofa
+
+
+def post42(url, payload):
+	url = "https://api.intra.42.fr" + url
+	payload = payload
+	headers = {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+	response = requests.request("POST", url, headers=headers, data=payload)
+	return response.json()
+
+def get42(url, payload, token):
+	url = "https://api.intra.42.fr" + url
+	payload = payload
+	headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': f'Bearer {token}'
+	}
+	response = requests.request("GET", url, headers=headers, data=payload)
+	return response.json()
+
+def get_token(CODE):
+	url = 'https://api.intra.42.fr/oauth/token'
+	UID = os.environ.get("UID_APP")
+	SECRET = os.environ.get("SECRET_KEY_APP")
+
+
+	data = {
+		'grant_type': 'authorization_code',
+		'client_id':  UID,
+		'client_secret': SECRET,
+		'code': CODE,
+		'redirect_uri': 'http://localhost:8000/twofa'
+	}
+
+	response = requests.post(url, data=data)
+	return response.json()['access_token']
+
+
+def create_user_API(code):
+	token = get_token(code)
+	if Users.objects.filter(token_42=hashlib.md5(token.encode()).hexdigest()).exists():
+		user = Users.objects.get(token_42=hashlib.md5(token.encode()).hexdigest())
+		return (user)
+
+def add_user_API(request):
+	code_from_user = request.GET.get('code')
+	user = create_user_API(code_from_user)
+	return user
 
 def authenticate_42(request):
 	UID = os.environ.get("UID_APP")
--- twofa/views.py
+++ twofa/views.py
@@ -1,61 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-import hashlib
-import requests
 from API.models import Users
-import os
-
-def post42(url, payload):
-	url = "https://api.intra.42.fr" + url
-	payload = payload
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-	response = requests.request("POST", url, headers=headers, data=payload)
-	return response.json()
-
-def get42(url, payload, token):
-	url = "https://api.intra.42.fr" + url
-	payload = payload
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Authorization': f'Bearer {token}'
-	}
-	response = requests.request("GET", url, headers=headers, data=payload)
-	return response.json()
-
-def get_token(CODE):
-	url = 'https://api.intra.42.fr/oauth/token'
-	UID = os.environ.get("UID_APP")
-	SECRET = os.environ.get("SECRET_KEY_APP")
-
-
-	data = {
-		'grant_type': 'authorization_code',
-		'client_id':  UID,
-		'client_secret': SECRET,
-		'code': CODE,
-		'redirect_uri': 'http://localhost:8000/twofa'
-	}
-
-	response = requests.post(url, data=data)
-	return response.json()['access_token']
-
-
-def create_user_API(code):
-	token = get_token(code)
-	if Users.objects.filter(token_42=hashlib.md5(token.encode()).hexdigest()).exists():
-		user = Users.objects.get(token_42=hashlib.md5(token.encode()).hexdigest())
-		return (user)
+from API.views import add_user_API
 
 
 
 def twofa(request):
 	try:
-		#add user to db
-		code_from_user = request.GET.get('code')
-		user = create_user_API(code_from_user)
+		#add user to db and return user of type Users
+		user = add_user_API(request)
 		#TODO twofa
 		return HttpResponse(f"Hello {user.username} , email {user.email}!")
 	except Exception as exc:
