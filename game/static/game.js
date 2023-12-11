71f801d63a4a486d182417177428c60226c85700
Arik Hativa <arikhativa@gmail.com>
2023-12-11 17:09:01 +0200

    done

--- game/static/game.js
+++ game/static/game.js
@@ -1,1 +1,79 @@
-// /asdasd
+import { GameController } from './GameController.js';
+import { ScreenManager } from './ScreenManager.js';
+import { Tournament } from './TournamentManager.js';
+
+// Define canvas and a context to draw to
+let canvas = document.getElementById("canvas");
+setCanvasSize();
+let ctx = canvas.getContext("2d");
+
+// Gmaejs
+function setCanvasSize() {
+    canvas.width = canvas.parentElement.clientWidth * 0.8;
+    canvas.height = canvas.parentElement.clientHeight * 0.8;
+    // console.log(canvas.width, canvas.height);
+}
+function onResize() {
+    setCanvasSize();
+}
+window.onresize = onResize;
+
+// Input handler
+function handleKeyPress(e, isKeyDown) {
+    if (game.p1 == null || game.p2 == null)
+        return ;
+    game.p1.move_listener(e, isKeyDown);
+    game.p2.move_listener(e, isKeyDown);
+}
+window.addEventListener('keydown', (e) => {
+    handleKeyPress(e, true);
+});
+window.addEventListener('keyup', (e) => {
+    handleKeyPress(e, false);
+});
+window.addEventListener('keyup', (e) => {
+    if (e.key !== " " || screenManager.transition)
+        return;
+    if (screenManager.currentScreen == screenManager.screens.GAME) {
+        game.pause = !game.pause;
+        return ;
+    } else if (screenManager.currentScreen == screenManager.screens.INTRO) {
+        screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE;
+    } else if (screenManager.currentScreen == screenManager.screens.VSSCREEN) {
+        screenManager.nextScreen = screenManager.screens.GAME;
+    } else if (screenManager.currentScreen == screenManager.screens.ENDOFMATCH) {
+        if (tournament.phaseChange)
+            screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE
+        else
+            screenManager.nextScreen = screenManager.screens.VSSCREEN;
+    } else if (screenManager.currentScreen == screenManager.screens.TOURNAMENTTREE) {
+        screenManager.nextScreen = screenManager.screens.VSSCREEN;
+    } else if (screenManager.currentScreen == screenManager.screens.ENDOFTOURNAMENT) {
+        //TODO: reset tournament
+    }
+    //TODO: new screens can be added here if you want to switch screens using space
+    screenManager.transition = true;
+});
+document.addEventListener('keydown', handleKeyPress);
+
+//Game SETUP
+let  tournament = new Tournament();
+let  game = new GameController(tournament.nextMatch());
+
+let screenManager = new ScreenManager();
+
+function gameLoop() {
+    //Clear Canvas
+    ctx.fillStyle = 'black';
+    ctx.globalAlpha = 1;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //Show screens
+    screenManager.loop(ctx, canvas, game, tournament);
+    //Manage the torunament
+    tournament.handler(game, screenManager);
+
+    // Request the next frame
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
--- spa/urls.py
+++ spa/urls.py
@@ -6,15 +6,18 @@ urlpatterns = [
     path("section/tournament/", views.tournament_view, name="tournament"),
     path("section/game/", views.game_view, name="game"),
     path("section/main/", views.main_view, name="main"),
-    path("section/validate_2fa_code/", views.validate_2fa_code, name="validate_2fa_code"),
+    path(
+        "section/validate_2fa_code/", views.validate_2fa_code, name="validate_2fa_code"
+    ),
     # these are to ALL of the page
     path("", views.spa_view, name="spa"),
     path("main/", views.spa_view, name="main"),
+    path("game/", views.spa_view, name="game"),
     path("twofa", views.spa_view, name="twofa"),
     path("API/authenticate_42", views.api_view, name="api"),
     path("twofa/qr_setup", views.spa_view, name="qr_setup"),
     path("twofa/email_setup", views.spa_view, name="email_setup"),
     path("validate_2fa_code/", views.spa_view, name="validate_2fa_code"),
-    path('set-language/<str:language_code>/', views.set_language, name='set_language'),
+    path("set-language/<str:language_code>/", views.set_language, name="set_language"),
     path("<path:catchall>", views.spa_view_catchall, name="spa"),
 ]
