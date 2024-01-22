0b3b7b93365dfd6e6b5a39fc0117da10f9d946de
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2024-01-22 13:45:32 +0100

    fixGamePageRace

--- django/src/navbar/static/navbar.js
+++ django/src/navbar/static/navbar.js
@@ -3,10 +3,9 @@ function readOppInput() {
     var elem = document.getElementById('opponent-name-input');
 
 	if (elem && elem.value)
-	{
 		showSection('game', {"players": elem.value});
-	}
-	showSection('game');
+	else 
+		showSection('game');
 }
 
 var playButton = document.getElementById("play-btn");
