e8fbc5993ed4056cd713e4460740abdcf946e646
Flamiing <amirlaaouam@gmail.com>
2024-01-23 22:54:52 +0100

    menu working, colors implementation still needs to be added

--- game/static/game.js
+++ game/static/game.js
@@ -2,38 +2,35 @@ import { GameController } from './GameController.js';
 import { ScreenManager } from './ScreenManager.js';
 import { Tournament } from './TournamentManager.js';
 
-const playersDataElement = document.getElementById('players-data');
-const playersDataString = playersDataElement ? playersDataElement.dataset.players : null;
-let playersList = [];
-
-if (playersDataString) {
-
-    const playersParam = new URLSearchParams(playersDataString).get('players');
-    
-    playersList = playersParam ? decodeURIComponent(playersParam).split(',') : [];
-}
-
-
 // Define canvas and a context to draw to
-let canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 setCanvasSize();
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+
+window.onresize = onResize;
+
+//Game SETUP
+let tournament;
+let game;
+let screenManager;
 
 // Gmaejs
 function setCanvasSize() {
     canvas.width = canvas.parentElement.parentElement.clientWidth * 0.8;
     canvas.height = canvas.parentElement.parentElement.clientHeight * 0.8;
-    // console.log(canvas.width, canvas.height);
+    //console.log(canvas.width, canvas.height);
 }
+
 function onResize() {
     setCanvasSize();
 }
-window.onresize = onResize;
 
-//Game SETUP
-let  tournament = new Tournament();
-let  game = new GameController(tournament.nextMatch(), {barriers: true, ballSpeed: 3, bonus: true});
-let screenManager = new ScreenManager();
+async function getGameSettings() {
+    const response = await fetch('/get_game_settings/');
+    const data = await response.json();
+    return data;
+}
 
 // Input handler
 function handleKeyPress(e, isKeyDown) {
@@ -42,36 +39,6 @@ function handleKeyPress(e, isKeyDown) {
     game.p1.move_listener(e, isKeyDown);
     game.p2.move_listener(e, isKeyDown);
 }
-window.addEventListener('keydown', (e) => {
-    handleKeyPress(e, true);
-});
-window.addEventListener('keyup', (e) => {
-    handleKeyPress(e, false);
-});
-window.addEventListener('keyup', (e) => {
-    if (e.key !== " " || screenManager.transition)
-        return;
-    if (screenManager.currentScreen == screenManager.screens.GAME) {
-        game.pause = !game.pause;
-        return ;
-    } else if (screenManager.currentScreen == screenManager.screens.INTRO) {
-        screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE;
-    } else if (screenManager.currentScreen == screenManager.screens.VSSCREEN) {
-        screenManager.nextScreen = screenManager.screens.GAME;
-    } else if (screenManager.currentScreen == screenManager.screens.ENDOFMATCH) {
-        if (tournament.phaseChange)
-            screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE
-        else
-            screenManager.nextScreen = screenManager.screens.VSSCREEN;
-    } else if (screenManager.currentScreen == screenManager.screens.TOURNAMENTTREE) {
-        screenManager.nextScreen = screenManager.screens.VSSCREEN;
-    } else if (screenManager.currentScreen == screenManager.screens.ENDOFTOURNAMENT) {
-        //TODO: reset tournament
-    }
-    //TODO: new screens can be added here if you want to switch screens using space
-    screenManager.transition = true;
-});
-document.addEventListener('keydown', handleKeyPress);
 
 function gameLoop() {
     //Clear Canvas
@@ -87,4 +54,68 @@ function gameLoop() {
     // Request the next frame
     requestAnimationFrame(gameLoop);
 }
-gameLoop();
+
+async function startGame() {
+    const playersDataElement = document.getElementById('players-data');
+    const playersDataString = playersDataElement ? playersDataElement.dataset.players : null;
+    let playersList = [];
+
+    if (playersDataString) {
+
+        const playersParam = new URLSearchParams(playersDataString).get('players');
+        
+        playersList = playersParam ? decodeURIComponent(playersParam).split(',') : [];
+    }
+
+
+    const userSettings = await getGameSettings();
+    tournament = new Tournament();
+    //game = new GameController(tournament.nextMatch(), {barriers: true, ballSpeed: 3, bonus: true});
+    game = new GameController(tournament.nextMatch(), {barriers: userSettings.walls, ballSpeed: 3, bonus: userSettings.bonus});
+    screenManager = new ScreenManager();
+
+
+    window.addEventListener('keydown', (e) => {
+        handleKeyPress(e, true);
+    });
+    window.addEventListener('keyup', (e) => {
+        handleKeyPress(e, false);
+    });
+    window.addEventListener('keyup', (e) => {
+        if (e.key !== " " || screenManager.transition)
+            return;
+        if (screenManager.currentScreen == screenManager.screens.GAME) {
+            game.pause = !game.pause;
+            return ;
+        } else if (screenManager.currentScreen == screenManager.screens.INTRO) {
+            screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE;
+        } else if (screenManager.currentScreen == screenManager.screens.VSSCREEN) {
+            screenManager.nextScreen = screenManager.screens.GAME;
+        } else if (screenManager.currentScreen == screenManager.screens.ENDOFMATCH) {
+            if (tournament.phaseChange)
+                screenManager.nextScreen = screenManager.screens.TOURNAMENTTREE
+            else
+                screenManager.nextScreen = screenManager.screens.VSSCREEN;
+        } else if (screenManager.currentScreen == screenManager.screens.TOURNAMENTTREE) {
+            screenManager.nextScreen = screenManager.screens.VSSCREEN;
+        } else if (screenManager.currentScreen == screenManager.screens.ENDOFTOURNAMENT) {
+            //TODO: reset tournament
+        }
+        //TODO: new screens can be added here if you want to switch screens using space
+        screenManager.transition = true;
+    });
+    document.addEventListener('keydown', handleKeyPress);
+
+    gameLoop();
+}
+
+if (document.readyState !== 'complete') {
+    window.addEventListener("DOMContentLoaded", (event) => {
+        console.log(event);
+        console.log("DOM fully loaded and parsed");
+        startGame();
+    });
+}
+else {
+    startGame();
+}
